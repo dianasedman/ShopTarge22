b15561804bfe323cfcfd7e0fba859b446b3bb298
Diana Sedman <dianasedman@gmail.com>
2023-10-07 13:51:07 +0300

    Models plus Layout

--- ShopTARge22/ShopTARge22/Models/KinderGartens/KindeergartenDeleteViewModel.cs
+++ ShopTARge22/ShopTARge22/Models/KinderGartens/KindeergartenDeleteViewModel.cs
@@ -2,5 +2,12 @@
 {
     public class KindeergartenDeleteViewModel
     {
+        public Guid Id { get; set; }
+        public string GroupName { get; set; }
+        public int ChildrenCount { get; set; }
+        public string KinderGartenName { get; set; }
+        public string Teacher { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
--- ShopTARge22/ShopTARge22/Models/KinderGartens/KindergartenIndexViewModel.cs
+++ ShopTARge22/ShopTARge22/Models/KinderGartens/KindergartenIndexViewModel.cs
@@ -2,5 +2,12 @@
 {
     public class KindergartenIndexViewModel
     {
+        public Guid Id { get; set; }
+        public string GroupName { get; set; }
+        public string KinderGartenName { get; set; }
+        public string Teacher {  get; set; }
+
     }
 }
+
+ 
